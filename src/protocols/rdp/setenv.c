283dae617df47c0a7c1bb6d3d43faacb9951b72c
Roman Procopie <roman@procopie.info>
2019-08-30 14:29:48 +0100

    changed new/delete to malloc/free as we are not C++

--- src/protocols/rdp/setenv.c
+++ src/protocols/rdp/setenv.c
@@ -3,10 +3,13 @@
 int setenv(const char *name, const char *value, int overwrite)
 {
     // alloc memory for name, equal sign, value and the terinating character
-    char *nameval = new char[strlen(name) + strlen(value) + 2)];
-    strcpy(nameval, name);
-    strcat(nameval,"=");
-    strcat(nameval,value);
-    putenv(strcat(name, value));
-    delete [] nameval;
+    char *nameval = (char *)malloc(sizeof(char)*(strlen(name) + strlen(value) + 2));
+    if (nameval != 0) // not null
+    {
+        strcpy(nameval, name);
+        strcat(nameval,"=");
+        strcat(nameval,value);
+        putenv(strcat(name, value));
+        free(nameval);
+    }
 }
